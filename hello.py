c2beb620cf87074d8f5b7294a09562ec73f83ed3
Li Yiyang <lyyyuna@gmail.com>
2016-11-16 17:26:40 +0800

    添加函数，启动时修改默认的 flask-bootstrap CDN 源。

--- hello.py
+++ hello.py
@@ -1,14 +1,40 @@
 from flask import Flask, render_template
 from flask_script import Manager
+from flask_bootstrap import Bootstrap, WebCDN, \
+    ConditionalCDN, BOOTSTRAP_VERSION, JQUERY_VERSION, HTML5SHIV_VERSION, RESPONDJS_VERSION
 
 app = Flask(__name__)
 
 manager = Manager(app)
+bootstrap = Bootstrap(app)
+
+
+def change_cdn_domestic(tar_app):
+    static = tar_app.extensions['bootstrap']['cdns']['static']
+    local = tar_app.extensions['bootstrap']['cdns']['local']
+
+    def change_one(tar_lib, tar_ver, fallback):
+        tar_js = ConditionalCDN('BOOTSTRAP_SERVE_LOCAL', fallback,
+            WebCDN('//cdn.bootcss.com/' + tar_lib + '/' + tar_ver + '/'))
+        tar_app.extensions['bootstrap']['cdns'][tar_lib] = tar_js
+
+    libs = {'jquery': {'ver': JQUERY_VERSION, 'fallback': local},
+            'bootstrap': {'ver': BOOTSTRAP_VERSION, 'fallback': local},
+            'html5shiv': {'ver': HTML5SHIV_VERSION, 'fallback': static},
+            'respond.js': {'ver': RESPONDJS_VERSION, 'fallback': static}}
+    for lib, par in libs.items():
+        change_one(lib, par['ver'], par['fallback'])
+change_cdn_domestic(app)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html')
 
 
 if __name__ == '__main__':
