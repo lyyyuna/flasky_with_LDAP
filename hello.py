f9bfe61ee49ddfec4950b3079a10a4476668e197
Li Yiyang <lyyyuna@gmail.com>
2016-11-17 11:35:07 +0800

    bootstrap base, moment 更换 url

--- hello.py
+++ hello.py
@@ -2,11 +2,15 @@ from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap, WebCDN, \
     ConditionalCDN, BOOTSTRAP_VERSION, JQUERY_VERSION, HTML5SHIV_VERSION, RESPONDJS_VERSION
+from flask_moment import Moment
+from datetime import datetime
+
 
 app = Flask(__name__)
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 def change_cdn_domestic(tar_app):
@@ -27,14 +31,24 @@ def change_cdn_domestic(tar_app):
 change_cdn_domestic(app)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html')
+    return render_template('user.html', name=name)
 
 
 if __name__ == '__main__':
