eb99ddf1bda399956c9ee700c57f015304612e01
lyyyuna <lyyyuna@gmail.com>
2016-11-20 17:44:14 +0800

    增加角色和用户模型

--- hello.py
+++ hello.py
@@ -1,25 +1,58 @@
+import os
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap, WebCDN, \
     ConditionalCDN, BOOTSTRAP_VERSION, JQUERY_VERSION, HTML5SHIV_VERSION, RESPONDJS_VERSION
 from flask_moment import Moment
-from datetime import datetime
+
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
+from flask_sqlalchemy import SQLAlchemy
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'boomshakalaka'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app) 
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+        
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
+
+
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+manager.add_command("shell", Shell(make_context=make_shell_context))
 
 
 def change_cdn_domestic(tar_app):
@@ -28,7 +61,7 @@ def change_cdn_domestic(tar_app):
 
     def change_one(tar_lib, tar_ver, fallback):
         tar_js = ConditionalCDN('BOOTSTRAP_SERVE_LOCAL', fallback,
-            WebCDN('//cdn.bootcss.com/' + tar_lib + '/' + tar_ver + '/'))
+                        WebCDN('//cdn.bootcss.com/' + tar_lib + '/' + tar_ver + '/'))
         tar_app.extensions['bootstrap']['cdns'][tar_lib] = tar_js
 
     libs = {'jquery': {'ver': JQUERY_VERSION, 'fallback': local},
@@ -54,12 +87,17 @@ def internal_server_error(e):
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'),
+                            known=session.get('known'), False)
 
 
 @app.route('/user/<name>')
