ca15254a7db5b2d67b69f1464ebe5c91afd1e016
Li Yiyang <lyyyuna@gmail.com>
2016-11-21 17:20:30 +0800

    修改程序结构，不再使用单文件。使用蓝图 blueprint

---
+++ app/__init__.py
@@ -0,0 +1,46 @@
+from config import config
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+
+from flask_bootstrap import Bootstrap, WebCDN, \
+    ConditionalCDN, BOOTSTRAP_VERSION, JQUERY_VERSION, HTML5SHIV_VERSION, RESPONDJS_VERSION
+
+bootstrap = Bootstrap()
+moment = Moment()
+db = SQLAlchemy()
+
+
+def change_cdn_domestic(tar_app):
+    static = tar_app.extensions['bootstrap']['cdns']['static']
+    local = tar_app.extensions['bootstrap']['cdns']['local']
+
+    def change_one(tar_lib, tar_ver, fallback):
+        tar_js = ConditionalCDN('BOOTSTRAP_SERVE_LOCAL', fallback,
+                        WebCDN('//cdn.bootcss.com/' + tar_lib + '/' + tar_ver + '/'))
+        tar_app.extensions['bootstrap']['cdns'][tar_lib] = tar_js
+
+    libs = {'jquery': {'ver': JQUERY_VERSION, 'fallback': local},
+            'bootstrap': {'ver': BOOTSTRAP_VERSION, 'fallback': local},
+            'html5shiv': {'ver': HTML5SHIV_VERSION, 'fallback': static},
+            'respond.js': {'ver': RESPONDJS_VERSION, 'fallback': static}}
+    for lib, par in libs.items():
+        change_one(lib, par['ver'], par['fallback'])
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    bootstrap.init_app(app)
+    change_cdn_domestic(app)
+
+    moment.init_app(app)
+    db.init_app(app)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
