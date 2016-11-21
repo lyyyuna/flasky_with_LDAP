029374a3f4582e44832336e1a5426105f71b7763
Li Yiyang <lyyyuna@gmail.com>
2016-11-21 10:44:18 +0800

    添加 vscode workspace settings. 及更新

--- hello.py
+++ hello.py
@@ -11,6 +11,7 @@ from wtforms.validators import Required
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
