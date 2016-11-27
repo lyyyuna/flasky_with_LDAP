f6c10d3f6fca9bdf8ba0cd23ed442c9880f0bad4
lyyyuna <lyyyuna@gmail.com>
2016-11-27 21:51:53 +0800

    了

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from . import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin, AnonymousUserMixin
+from datetime import datetime
 
 
 class Permission:
