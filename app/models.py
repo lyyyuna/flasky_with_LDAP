4169123d2357ba3118f266bad299f4e1ed6438bd
lyyyuna <lyyyuna@gmail.com>
2016-11-30 21:17:50 +0800

    前台编辑实时渲染，后台markdown 有改动时渲染。

--- app/models.py
+++ app/models.py
@@ -2,6 +2,8 @@ from . import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin, AnonymousUserMixin
 from datetime import datetime
+from markdown import markdown
+import bleach
 
 
 class Permission:
@@ -132,6 +134,7 @@ class Post(db.Model):
     __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text)
+    body_html = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
@@ -149,3 +152,15 @@ class Post(db.Model):
                     author=u)
             db.session.add(p)
             db.session.commit()
+
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
+                        'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
+                        'h1', 'h2', 'h3', 'p']
+        target.body_html = bleach.linkify(bleach.clean(
+            markdown(value, output_format='html'),
+            tags=allowed_tags, strip=True
+        ))
+
+db.event.listen(Post.body, 'set', Post.on_changed_body)
